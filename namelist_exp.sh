fd6523e221bb471d083e9dc9293596113d259def
Mathieu LE CORRE <mlecorre@datarmor2.ib0.ice.ifremer.fr>
2021-06-22 14:03:24 +0000

    some cleaning

--- namelist_exp.sh
+++ namelist_exp.sh
@@ -62,12 +62,11 @@ export OCE_XIOS_NAME="${CONFIG}_1d_aver ${CONFIG}_1h_inst_surf ${CONFIG}_6h_his"
 MPI_LAUNCH_CMD=$MPI_LAUNCH  # ccc_mprun :for irene / $MPI_LAUCH for datarmor (or mpiexec.hydra )
 
 ### PROX XIOS ###
-export NP_XIOS_ATM=1 # 4
+export NP_XIOS_ATM=1
 export NP_XIOS_OCE=1
 
 ### PROC CROCO ###
 export NP_CRO=4
-#export JPNJ=32
 
 ### PROC WRF ###
 export NP_WRF=12
@@ -90,7 +89,6 @@ export SERIAL_LAUNCH_WW3="$MPI_LAUNCH -n 1 "
 export DEBUG="FALSE"     
 export SCRIPT_DEBUG="FALSE"     
 export LEVITUS=0  # for init state  ... si demarrage climato (levitus ou autre)
-export TRDMLD=0
 
 #-------------------------------------------------------------------------------
 #  1 experiment / n jobs
@@ -132,11 +130,10 @@ export TSP_WW_PRO=1200  # TCFL --> ww3.grid to see the definition
 export TSP_WW_REF=1800  # TMAX / 2
 export TSP_WW_SRC=10
 
-### ICE ###
-export TSP_ICE=-1
 
+### CPL ###
 export CPL_FREQ=21600
-export TIMEsnd=20000 # NO NEED on datarmor
+
 
 #-------------------------------------------------------------------------------
 # Grids sizes
@@ -162,13 +159,12 @@ export wrf_diag_int_m=$((${wrf_his_h}*60))  # diag output interval (m)
 export wrf_diag_frames=1000 #$wrf_his_frames    # nb of diag outputs per file
 
 # CROCO
-#export oce_nrst=144  # in namcroco rst is equal to ocean time  # restart interval (in number of timesteps) 
+
 export oce_nhis=18     # history output interval (in number of timesteps) 
 export oce_navg=18     # average output interval (in number of timesteps) 
 
 # WW3
 export wav_int=21600            # output interval (s)
-# wav_rst=$((24*3600))    # restart interval (s) 
 # ww3 file to be used for creating restart file for oasis 
 export ww3file=/home2/datawork/mlecorre/COUPLING/CONFIG/BENGUELA/outputs_frc_ww3_CFSR/ww3.200501.nc # Usually done by running a frc mode on the area
 
