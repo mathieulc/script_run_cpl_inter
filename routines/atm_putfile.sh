4465c69268561e95529db93ddd6fae992ed82f78
mathieulc <mathieu.lecorre@univ-brest.fr>
2021-08-09 14:26:53 +0000

    small modif

--- routines/atm_putfile.sh
+++ routines/atm_putfile.sh
@@ -10,9 +10,9 @@ if [ ${USE_XIOS_ATM} -eq 1 ] ; then
 else
     module load $ncomod
     for dom in `seq 1 $NB_dom`; do
-        ncea -O -F -d time,1,-1 wrfout_d0${dom}_${YEAR_BEGIN_JOB}-* wrfout_d0${dom}_${DATE_BEGIN_JOB}_${DATE_END_JOB}.nc
-        \rm wrfout_d0${dom}_${YEAR_BEGIN_JOB}-*        
+        ncea -O -F -d Time,1,-1 wrfout_d0${dom}_${YEAR_BEGIN_JOB}-* wrfout_d0${dom}_${DATE_BEGIN_JOB}_${DATE_END_JOB}.nc
         mv wrfout_d0${dom}_${DATE_BEGIN_JOB}_${DATE_END_JOB}.nc ${OUTPUTDIR}/.
+        \rm wrfout_d0${dom}_${YEAR_BEGIN_JOB}-*
         mv wrfxtrm_d0${dom}_${YEAR_BEGIN_JOB}-* ${OUTPUTDIR}/wrfxtrm_d0${dom}_${DATE_BEGIN_JOB}_${DATE_END_JOB}.nc
    done
    module unload $ncomod
